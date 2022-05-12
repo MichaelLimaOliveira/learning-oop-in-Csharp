e181bfa13a482c801ca4cb3edce140d79e39059e
Michael Lima <michaellmo@outlook.com>
2022-05-12 16:44:07 -0300

    inicio do curso POO

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Globalization;
+
+string[] firstEnter = Console.ReadLine().Split(' ');
+string[] secondEnter = Console.ReadLine().Split(' ');
+
+double coutFirst = double.Parse(firstEnter[1], CultureInfo.InvariantCulture);
+double coutSecond = double.Parse(secondEnter[1], CultureInfo.InvariantCulture);
+
+double firstPrice = double.Parse(firstEnter[2], CultureInfo.InvariantCulture);
+double secondPrice = double.Parse(secondEnter[2], CultureInfo.InvariantCulture);
+
+double totalValue = (coutFirst * firstPrice) + (coutSecond * secondPrice);
+
+Console.WriteLine("Total a pagar: " +totalValue.ToString("F2", CultureInfo.InvariantCulture));
