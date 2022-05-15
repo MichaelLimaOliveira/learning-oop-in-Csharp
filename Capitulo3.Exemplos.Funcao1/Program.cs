ab12f6c9e42b46dc6c527104164b07fe541f18d4
Michael Lima <michaellmo@outlook.com>
2022-05-14 21:41:30 -0300

    atividades novas

---
+++ Capitulo3.Exemplos.Funcao1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Capitulo3.Exemplos.Funcao1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite os 3 numeros para comparar (Um apos o outros): ");
+            int numberA = int.Parse(Console.ReadLine());
+            int numberB = int.Parse(Console.ReadLine());
+            int numberC = int.Parse(Console.ReadLine());
+
+            int result;
+            Console.WriteLine($"O maior entre os 3 e: {result = MaxNumber(numberA, numberB, numberC)}");
+
+            Console.Write("Digite quantos valores quiser para testar o maior (Todos na mesma linha): ");
+            int[] vet = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(result = Maior(vet));
+
+            Console.Write("Digite quantos valores quiser para testar o menor (Todos na mesma linha): ");
+            vet = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(result = Minimal(vet));
+
+        }
+
+        static int MaxNumber(int a, int b, int c)
+        {
+            int _maxNumber;
+            if (a > b && a > c)
+                _maxNumber = a;
+            else if (b > c)
+                _maxNumber = b;
+            else
+                _maxNumber = c;
+
+            return _maxNumber;
+        }
+
+        static int Maior(int[] vet)
+        {
+            int _max = vet[0];
+            for (int i = 0; i < vet.Length; i++)
+            {
+                if (vet[i] > _max)
+                    _max = vet[i];
+            }
+            return _max;
+        }
+
+        static int Minimal(int[] vet)
+        {
+            int _minimal = vet[0];
+            for(int i = 0; i < vet.Length; i++)
+            {
+                if (vet[vet[i]] < _minimal)
+                    _minimal = vet[i];
+            }
+            return _minimal;
+        }
+    }
+}
---
+++ Capitulo3.Exercicio8/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Capitulo3.Exercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio1");
+
+            int code = int.Parse(Console.ReadLine());
+            PassWorld(code);
+
+            Console.WriteLine("Exercicio2");
+
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Esse sao os numeros impar de 1 ate {number}:");
+            AllOddNumber(number);
+        }
+
+        static void PassWorld(int code)
+        {
+            const int _realCode = 2002;
+
+            while (code != _realCode)
+            {
+                Console.WriteLine("Senha Invalida");
+                code = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Senha correta");
+        }
+
+        static void  AllOddNumber (int limitValue)
+        {
+            for (int i = 1; i < limitValue; i++)
+                if (i % 2 != 0)
+                    Console.WriteLine(i);
+            
+        
+        }
+    }
+}
