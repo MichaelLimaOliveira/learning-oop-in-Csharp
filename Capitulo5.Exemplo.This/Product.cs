d9dd9ee324a332a94a0b636c9b6b51aee64feb0e
Michael Lima <michaellmo@outlook.com>
2022-05-19 21:04:14 -0300

    Melhorias no jogo e atividades novas

---
+++ Capitulo5.Exemplo.This/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Capitulo5.Exemplo.This
+{
+    public class Product
+    {
+        public string Name { get; protected set; }
+        private decimal _price;
+        public decimal SellPrice { get; protected set; }
+        public string CodeBar { get; protected set; }
+        public int Amount { get; protected set; }
+
+        public Product(string name, decimal price, string codeBar, int amount)
+        {
+            SetName(name);
+            SetProductPrice(price);
+            CodeBar = codeBar;
+            Amount = amount;
+        }
+
+        public void SetProductPrice(decimal price)
+        {
+            if (price == 0)
+            {
+                Console.WriteLine("Valor não pode ser zero!");
+                return;
+            }
+
+            _price = price;
+            SellPrice = _price + (_price * 0.30m);
+        }
+
+        public void SetName(string name)
+        {
+            name = name.Replace(" ", "");
+            if (name != null) Name = name.ToUpper();
+        }
+
+        public void AddProduct(int quantity) => Amount += quantity;
+        public void RemoveProduct(int quantity) => Amount -= quantity;
+
+        public override string ToString()
+        {
+            return Name + " " + SellPrice.ToString("F2") + " " + CodeBar + " " + Amount;
+        }
+
+    }
+}
---
+++ Capitulo5.Exemplo.This/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Capitulo5.Exemplo.This
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product geladeira = new Product("Geladeira primor", 1200.00m, "0001", 50);
+
+            Console.WriteLine(geladeira.ToString());
+
+
+        }
+    }
+}
