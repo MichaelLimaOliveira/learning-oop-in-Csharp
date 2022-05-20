682d508c5e2176ae3cb23518c57aceadc6345df0
Michael Lima <michaellmo@outlook.com>
2022-05-20 20:10:08 -0300

    Exercicio Conta Bancaria

---
+++ Capitulo5.Exercicio.ContaBancaria/Account.cs
@@ -0,0 +1,38 @@
+﻿namespace Capitulo5.Exercicio.ContaBancaria
+{
+    internal class Account
+    {
+        private double _tax = 5.00;
+        public string ID { get; }
+        public string Name { get; private set; }
+
+        public double Balance { get; private set; }
+
+        public Account(string iD, string name)
+        {
+            ID = iD;
+            Name = name;
+        }
+        public Account(string iD, string name, double balance)
+        {
+            ID = iD;
+            Name = name;
+            Balance = balance;
+        }
+
+        public void Deposit(double quantity)
+        {
+            Balance += quantity;
+        }
+
+        public void DrawMoney(double quantity)
+        {
+            Balance -= quantity + _tax;
+        }
+
+        public void ChangeName(string name)
+        {
+            if (name != this.Name && name != null) this.Name = name;
+        }
+    }
+}
