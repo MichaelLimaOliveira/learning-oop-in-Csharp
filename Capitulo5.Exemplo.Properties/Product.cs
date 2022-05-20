7565413e4339327d07c02500fff3eeb6a49cc556
Michael Lima <michaellmo@outlook.com>
2022-05-20 17:06:48 -0300

    Atividades Propriedades

---
+++ Capitulo5.Exemplo.Properties/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Capitulo5.Exemplo.Properties
+{
+    internal class Product
+    {
+        private string _name;
+
+        public int Amount { get; private set; }
+
+        public double Price { get; private set; }
+
+        public Product(string name, int amount, double price)
+        {
+            _name = name.ToUpper();
+            Amount = amount;
+            Price = price + (price * 0.30);
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { if (value != null && value.Length > 1) _name = value; }
+        }
+
+        //public double Price => _price;
+
+        //public int Amount => _amount;
+
+        public void AddAmount(int quantity)
+        {
+            if (quantity > 0) Amount += quantity;
+        }
+
+        public void RemoveAmount(int quantity)
+        {
+            if (quantity > 0) Amount -= quantity;
+        }
+
+        public double TotalStockValue() => Price * Amount;
+
+        public override string ToString()
+        {
+            return "Name: " + Name
+                            + ", Preço: "
+                            + Price.ToString("F2");
+        }
+    }
+}
---
+++ Capitulo5.Exemplo.Properties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Capitulo5.Exemplo.Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product("tV", 10, 500.00);
+
+            Console.WriteLine(p1.Name);
+            Console.WriteLine(p1.Price);
+            Console.WriteLine(p1.Amount);
+
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p1.TotalStockValue().ToString("F2"));
+
+
+        }
+    }
+}
